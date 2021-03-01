b2bc37ad3fa79511969423b23c45b4ed03fd360f
Hansatang <portokarlos@gmail.com>
2021-03-01 14:42:33 +0100

    Pacierz 01/03

---
+++ src/temperature/mediator/radiator/RadiatorModelManager.java
@@ -0,0 +1,18 @@
+package temperature.mediator.radiator;
+
+import java.beans.PropertyChangeListener;
+
+public class RadiatorModelManager implements RadiatorModel
+{
+  @Override public void addListener(String propertyName,
+      PropertyChangeListener listener)
+  {
+
+  }
+
+  @Override public void removeListener(String propertyName,
+      PropertyChangeListener listener)
+  {
+
+  }
+}
