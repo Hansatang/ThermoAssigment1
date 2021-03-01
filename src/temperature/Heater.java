905c26e7fa289b86a878c8c3696c4efb8f8b9746
Fogh1206 <esbenvensel.fogh@gmail.com>
2021-03-01 14:19:58 +0100

    Init

---
+++ src/temperature/Heater.java
@@ -0,0 +1,15 @@
+package temperature;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public class Heater implements PropertyChangeListener {
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+
+    }
+
+
+
+}
